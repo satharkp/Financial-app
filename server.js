df3f22da45660fc2a5c67e2cb35c0bb64a7ae96b
Apple <apple@Apples-MacBook-Pro.local>
2026-01-18 15:45:00 +0530

    asthetic changed

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+app.options("*", cors());
 
 app.use(express.json());
 
